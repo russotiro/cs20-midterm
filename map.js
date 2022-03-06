4ff5bc9bb90a63f5b947b91fbe68a7a87d875f4f
Henry Gray <hgray2020@francisparker.org>
2022-03-06 14:48:57 -0500

    added map to tickets.html

--- map.js
+++ map.js
@@ -1,62 +1,85 @@
+var costPerKM = 3;
+var busTypeMultiplier = [1, 1.2, 1.4, 1.8];
+
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: {
-            lat: -24.345,
-            lng: 134.46
-        }, // Australia.
-    });
     const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer({
-        draggable: true,
-        map,
-        panel: document.getElementById("panel"),
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: { lat: 42.42, lng: -71.11 },
     });
+    console.log("wa");
 
-    directionsRenderer.addListener("directions_changed", () => {
-        const directions = directionsRenderer.getDirections();
 
-        if (directions) {
-            computeTotalDistance(directions);
-        }
-    });
-    displayRoute(
-        "Perth, WA",
-        "Sydney, NSW",
-        directionsService,
-        directionsRenderer
-    );
 }
 
-function displayRoute(origin, destination, service, display) {
-    service
+function calculateAndDisplayRoute(form, directionsService, directionsRenderer, start, dest) {
+    directionsService
         .route({
-            origin: origin,
-            destination: destination,
-            waypoints: [],
+            origin: {
+                query: start,
+            },
+            destination: {
+                query: dest,
+            },
             travelMode: google.maps.TravelMode.DRIVING,
-            avoidTolls: true,
         })
-        .then((result) => {
-            display.setDirections(result);
+        .then((response) => {
+            directionsRenderer.setDirections(response);
+            var dist = computeTotalDistance(response);
+            calcAndDisplayCost(form, dist);
         })
-        .catch((e) => {
-            alert("Could not display directions due to: " + e);
-        });
+        .catch((e) => window.alert("Directions request failed due to " + status));
+}
+
+function handleForm(form) {
+
+    document.getElementById("map").setAttribute("style", "margin:2vw;height:60%;");
+
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: { lat: 42.42, lng: -71.11 },
+    });
+
+    directionsRenderer.setMap(map);
+    calculateAndDisplayRoute(form, directionsService, directionsRenderer, form.start.value, form.end.value);
+
 }
 
 function computeTotalDistance(result) {
-    let total = 0;
-    const myroute = result.routes[0];
 
-    if (!myroute) {
+    let total = 0;
+    const myRoute = result.routes[0];
+
+    if (!myRoute) {
         return;
     }
 
-    for (let i = 0; i < myroute.legs.length; i++) {
-        total += myroute.legs[i].distance.value;
+    for (let i = 0; i < myRoute.legs.length; i++) {
+        total += myRoute.legs[i].distance.value;
+
     }
 
     total = total / 1000;
-    document.getElementById("total").innerHTML = total + " km";
+    return total;
+}
+
+function calcAndDisplayCost(form, dist) {
+    var mult = 1;
+    if (form.roundtrip[0].checked) {
+        mult = 2;
+    }
+
+    var e = document.getElementById("buses");
+    var bus = e.options[e.selectedIndex].value;
+    mult = mult * busTypeMultiplier[parseInt(bus)];
+
+    var cost = mult * costPerKM * dist;
+
+    cost = Math.round(cost);
+    document.getElementById("cost").innerHTML = "<p>Estimated Cost: $" + cost + "</p>";
+
 }
